eaffc6e851b094d6a91e4d040a35e3b30cb01ce9
GRASSETS TECH LIMITED <82155440+grassets-tech@users.noreply.github.com>
2022-09-14 00:46:53 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,27 @@
-apk add --update --no-cache openssh 
-echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
-adduser -h /home/blockpi -s /bin/sh -D blockpi
-echo -n 'blockpi:blockpi' | chpasswd
-ssh-keygen -A
-exec /usr/sbin/sshd -D -e "$@"
+#!/bin/bash
+
+# my_name=davaymne
+# my_beacon=1.1.1.1
+# my_erc_20=0x000
+# hypernode_password=test1
+# my_root_password=test1
+
+#(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+# service ssh restart
+
+mkdir ~/HyperNode
+cd ~/HyperNode
+version=`wget -qO- -t1 -T2 "https://api.github.com/repos/BlockPILabs/testnets/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
+wget https://github.com/BlockPILabs/testnets/releases/download/${version}/HyperNode
+wget https://github.com/BlockPILabs/testnets/releases/download/${version}/config.yml
+chmod +x ./HyperNode
+mv ./HyperNode /usr/local/bin/
+
+(echo ${hypernode_password}; echo ${hypernode_password}) | HyperNode init
+echo ${hypernode_password} > ~/HyperNode/passwd.txt
+
+key_file=$(ls ~/HyperNode/keystore/ -t -U | grep -m 1 "UTC")
+
+sed -i 's/erc20 address/${my_erc_20}/' ~/HyperNode/config.yml
+sed -i 's/{beacon}/${my_beacon}/' ~/HyperNode/config.yml
+sed -i 's/unknown/${my_name}/' ~/HyperNode/config.yml
